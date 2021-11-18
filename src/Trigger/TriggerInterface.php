afc80f9af7154b4a4b2145a41e832cd1b8cc04c8
ygh <admin@haoguangyun.com>
2021-11-18 15:24:41 +0800

    bug fix

--- src/Trigger/TriggerInterface.php
+++ src/Trigger/TriggerInterface.php
@@ -6,6 +6,6 @@ namespace EasySwoole\EasySwoole\Trigger;
 
 interface TriggerInterface
 {
-    public function error($msg,int $errorCode = E_USER_ERROR,Location $location = null);
+    public function error($msg, int $errorCode = E_USER_ERROR, $file = null, $line = null);
     public function throwable(\Throwable $throwable);
 }
